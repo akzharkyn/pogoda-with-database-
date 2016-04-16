11a08c3d9d9f512b330c82e5f808ef308e53ce09
akzharkyn.s <akzharkyn.s@mail.ru>
2016-04-17 01:45:34 +0600

    pogoda with database

---
+++ pogoda/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace pogoda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label_Uralsk = new System.Windows.Forms.Label();
+            this.label_Aktobe = new System.Windows.Forms.Label();
+            this.label_Atyrau = new System.Windows.Forms.Label();
+            this.label_Aktau = new System.Windows.Forms.Label();
+            this.label_Kyzylorda = new System.Windows.Forms.Label();
+            this.label_Petropavlovsk = new System.Windows.Forms.Label();
+            this.label_Shymkent = new System.Windows.Forms.Label();
+            this.label_Taraz = new System.Windows.Forms.Label();
+            this.label_Karaganda = new System.Windows.Forms.Label();
+            this.label_Kokshetau = new System.Windows.Forms.Label();
+            this.label_Kostanay = new System.Windows.Forms.Label();
+            this.label_Almaty = new System.Windows.Forms.Label();
+            this.label_Ustkamenogorsk = new System.Windows.Forms.Label();
+            this.label_Pavlodar = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_Uralsk
+            // 
+            this.label_Uralsk.Location = new System.Drawing.Point(26, 97);
+            this.label_Uralsk.Name = "label_Uralsk";
+            this.label_Uralsk.Size = new System.Drawing.Size(168, 33);
+            this.label_Uralsk.TabIndex = 0;
+            this.label_Uralsk.Text = "oral";
+            // 
+            // label_Aktobe
+            // 
+            this.label_Aktobe.Location = new System.Drawing.Point(212, 189);
+            this.label_Aktobe.Name = "label_Aktobe";
+            this.label_Aktobe.Size = new System.Drawing.Size(164, 33);
+            this.label_Aktobe.TabIndex = 1;
+            this.label_Aktobe.Text = "aktobe";
+            // 
+            // label_Atyrau
+            // 
+            this.label_Atyrau.Location = new System.Drawing.Point(10, 210);
+            this.label_Atyrau.Name = "label_Atyrau";
+            this.label_Atyrau.Size = new System.Drawing.Size(184, 33);
+            this.label_Atyrau.TabIndex = 2;
+            this.label_Atyrau.Text = "atyrau";
+            // 
+            // label_Aktau
+            // 
+            this.label_Aktau.Location = new System.Drawing.Point(45, 374);
+            this.label_Aktau.Name = "label_Aktau";
+            this.label_Aktau.Size = new System.Drawing.Size(163, 33);
+            this.label_Aktau.TabIndex = 3;
+            this.label_Aktau.Text = "aktau";
+            // 
+            // label_Kyzylorda
+            // 
+            this.label_Kyzylorda.Location = new System.Drawing.Point(306, 387);
+            this.label_Kyzylorda.Name = "label_Kyzylorda";
+            this.label_Kyzylorda.Size = new System.Drawing.Size(168, 33);
+            this.label_Kyzylorda.TabIndex = 4;
+            this.label_Kyzylorda.Text = "Kyzylorda";
+            // 
+            // label_Petropavlovsk
+            // 
+            this.label_Petropavlovsk.Location = new System.Drawing.Point(454, 9);
+            this.label_Petropavlovsk.Name = "label_Petropavlovsk";
+            this.label_Petropavlovsk.Size = new System.Drawing.Size(158, 33);
+            this.label_Petropavlovsk.TabIndex = 5;
+            this.label_Petropavlovsk.Text = "petropavlovsk";
+            // 
+            // label_Shymkent
+            // 
+            this.label_Shymkent.Location = new System.Drawing.Point(523, 387);
+            this.label_Shymkent.Name = "label_Shymkent";
+            this.label_Shymkent.Size = new System.Drawing.Size(187, 33);
+            this.label_Shymkent.TabIndex = 6;
+            this.label_Shymkent.Text = "Shymkent";
+            // 
+            // label_Taraz
+            // 
+            this.label_Taraz.Location = new System.Drawing.Point(589, 340);
+            this.label_Taraz.Name = "label_Taraz";
+            this.label_Taraz.Size = new System.Drawing.Size(190, 33);
+            this.label_Taraz.TabIndex = 7;
+            this.label_Taraz.Text = "taraz";
+            // 
+            // label_Karaganda
+            // 
+            this.label_Karaganda.Location = new System.Drawing.Point(509, 210);
+            this.label_Karaganda.Name = "label_Karaganda";
+            this.label_Karaganda.Size = new System.Drawing.Size(180, 33);
+            this.label_Karaganda.TabIndex = 8;
+            this.label_Karaganda.Text = "karagandy";
+            this.label_Karaganda.Click += new System.EventHandler(this.label_Karaganda_Click);
+            // 
+            // label_Kokshetau
+            // 
+            this.label_Kokshetau.Location = new System.Drawing.Point(512, 108);
+            this.label_Kokshetau.Name = "label_Kokshetau";
+            this.label_Kokshetau.Size = new System.Drawing.Size(143, 33);
+            this.label_Kokshetau.TabIndex = 9;
+            this.label_Kokshetau.Text = "kokshetau";
+            // 
+            // label_Kostanay
+            // 
+            this.label_Kostanay.Location = new System.Drawing.Point(280, 68);
+            this.label_Kostanay.Name = "label_Kostanay";
+            this.label_Kostanay.Size = new System.Drawing.Size(194, 33);
+            this.label_Kostanay.TabIndex = 10;
+            this.label_Kostanay.Text = "kostanay";
+            // 
+            // label_Almaty
+            // 
+            this.label_Almaty.Location = new System.Drawing.Point(788, 288);
+            this.label_Almaty.Name = "label_Almaty";
+            this.label_Almaty.Size = new System.Drawing.Size(180, 33);
+            this.label_Almaty.TabIndex = 11;
+            this.label_Almaty.Text = "almaty";
+            // 
+            // label_Ustkamenogorsk
+            // 
+            this.label_Ustkamenogorsk.Location = new System.Drawing.Point(805, 210);
+            this.label_Ustkamenogorsk.Name = "label_Ustkamenogorsk";
+            this.label_Ustkamenogorsk.Size = new System.Drawing.Size(178, 33);
+            this.label_Ustkamenogorsk.TabIndex = 12;
+            this.label_Ustkamenogorsk.Text = "oskemen";
+            // 
+            // label_Pavlodar
+            // 
+            this.label_Pavlodar.Location = new System.Drawing.Point(696, 47);
+            this.label_Pavlodar.Name = "label_Pavlodar";
+            this.label_Pavlodar.Size = new System.Drawing.Size(161, 33);
+            this.label_Pavlodar.TabIndex = 13;
+            this.label_Pavlodar.Text = "pavlodar";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(244, 447);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 45);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Show Wheather";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(995, 521);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label_Pavlodar);
+            this.Controls.Add(this.label_Ustkamenogorsk);
+            this.Controls.Add(this.label_Almaty);
+            this.Controls.Add(this.label_Kostanay);
+            this.Controls.Add(this.label_Kokshetau);
+            this.Controls.Add(this.label_Karaganda);
+            this.Controls.Add(this.label_Taraz);
+            this.Controls.Add(this.label_Shymkent);
+            this.Controls.Add(this.label_Petropavlovsk);
+            this.Controls.Add(this.label_Kyzylorda);
+            this.Controls.Add(this.label_Aktau);
+            this.Controls.Add(this.label_Atyrau);
+            this.Controls.Add(this.label_Aktobe);
+            this.Controls.Add(this.label_Uralsk);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_Uralsk;
+        private System.Windows.Forms.Label label_Aktobe;
+        private System.Windows.Forms.Label label_Atyrau;
+        private System.Windows.Forms.Label label_Aktau;
+        private System.Windows.Forms.Label label_Kyzylorda;
+        private System.Windows.Forms.Label label_Petropavlovsk;
+        private System.Windows.Forms.Label label_Shymkent;
+        private System.Windows.Forms.Label label_Taraz;
+        private System.Windows.Forms.Label label_Karaganda;
+        private System.Windows.Forms.Label label_Kokshetau;
+        private System.Windows.Forms.Label label_Kostanay;
+        private System.Windows.Forms.Label label_Almaty;
+        private System.Windows.Forms.Label label_Ustkamenogorsk;
+        private System.Windows.Forms.Label label_Pavlodar;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ pogoda/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace pogoda
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            //city.Add("label_Ustkamenogorsk".Split('_'),Name.Split('_'));
+
+
+
+        }
+        static Dictionary<string, string[]> data = new Dictionary<string, string[]>();
+        Dictionary<string, string[]> city = new Dictionary<string, string[]>()
+;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c.GetType() == typeof(Label))
+                {
+                    string name = c.Name;
+                    //if(city.ContainsKey(name))
+                    //{
+                    //    name = city[name];
+                    //}
+                    name= name.Split('_')[1].ToLower();
+                    //MessageBox.Show(c.Name.Split('_')[1]);
+                    //string name = c.Name.Split('_')[1].ToLower();
+                    string url = string.Format("http://www.kazhydromet.kz/rss-pogoda.php?id={0}", name);
+                    XmlReader reader = XmlReader.Create(url);
+
+
+                    SyndicationFeed feed = SyndicationFeed.Load(reader);
+
+                    reader.Close();
+
+                    if (feed.Items.Count() > 0)
+                    {
+                        string text = feed.Items.ElementAt(0).Summary.Text;
+
+                        string[] arr = text.Split(new string[] { "<br>" }, StringSplitOptions.RemoveEmptyEntries);
+
+                        if (arr.Length >= 4)
+                        {
+                            for (int i = 0; i < arr.Length; ++i)
+                            {
+                                arr[i] = arr[i].Trim();
+                            }
+
+                            data[name] = arr;
+                        }
+                        c.Text = data[name][0];
+
+                        if(label_Ustkamenogorsk.Text=="oskemen")
+                        {
+                            label_Ustkamenogorsk.Name = "label_Ust-kamenogorsk";
+                        }
+                       
+
+                    }
+
+                }
+            }
+        }
+
+        private void label_Karaganda_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
